96926e57163af37022bdf313edf3f358fdf2c9c2
izabela08 <izabelalima17@gmail.com>
2022-08-04 18:21:36 -0300

    exercicio aula012

---
+++ script.js
@@ -0,0 +1,12 @@
+let media = 7
+
+    if(media >= 5){
+        console.log('APROVADO')
+    }else if(media >= 3){
+        console.log('REPROVADO')
+    }else if(media < 3){
+        console.log('RECUPERAÇÃO')
+    }
+    else{
+        console.log("DADOS INVALIDOS")
+    }
